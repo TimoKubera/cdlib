12c19d5cb5c007e936c7b262eac0262277358e93
TechDebotBot <tech-debt-bot@reply.de>
2025-03-17 18:35:59 +0100

    TechDebtBot-refactor:[kotlin:S1192] Define constant for SHA-256

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
@@ -1,19 +1,23 @@
 package de.deutschepost.sdm.cdlib.utils
 
+import kotlin.text.toByteArray
 import java.io.File
 import java.security.MessageDigest
 
 fun String.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
-    .digest(this.toByteArray())
-    .fold("") { str, it -> str + "%02x".format(it) }
-
-fun File.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
-    .digest(this.readBytes())
-    .fold("") { str, it -> str + "%02x".format(it) }
-
-fun ByteArray.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    const val SHA_256 = "SHA-256"
+    
+    fun String.sha256sum(): String = MessageDigest
+        .getInstance(SHA_256)
+        .digest(this.toByteArray())
+        .fold("") { str, it -> str + "%02x".format(it) }
+    
+    fun File.sha256sum(): String = MessageDigest
+        .getInstance(SHA_256)
+        .digest(this.readBytes())
+        .fold("") { str, it -> str + "%02x".format(it) }
+    
+    fun ByteArray.sha256sum(): String = MessageDigest
+        .getInstance(SHA_256)
     .digest(this)
     .fold("") { str, it -> str + "%02x".format(it) }
