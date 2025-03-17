2eab3c306fdb9d27ba861c7822dbdca021f753a7
TechDebotBot <tech-debt-bot@reply.de>
2025-03-17 19:12:39 +0100

    TechDebtBot-refactor:[kotlin:S6624]Extract version constant

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -56,7 +56,9 @@ dependencies {
     // webapproval sharepoint
     const val httpClientVersion = "4.5.14"
     implementation("org.apache.httpcomponents:httpclient:$httpClientVersion")
-    implementation("org.codelibs:jcifs:1.3.18.3") // Never update this, Version 2 is incompatible
+    const val jcifsVersion = "1.3.18.3"
+    
+    implementation("org.codelibs:jcifs:$jcifsVersion") // Never update this, Version 2 is incompatible
     // JGit (https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit)
     implementation("org.eclipse.jgit:org.eclipse.jgit:7.1.0.202411261347-r")
     //publish metrics to azure cosmosdb
