1d68be6897aee6bd153b9a4f5dafb90592c25584
TechDebotBot <tech-debt-bot@reply.de>
2025-03-17 17:52:47 +0100

    TechDebtBot-fix:[kotlin:S108] Add comment to empty catch block

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/archive/ArchiveCommandIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/archive/ArchiveCommandIntegrationTest.kt
@@ -174,6 +174,7 @@ class ArchiveCommandIntegrationTest(@Value("\${artifactory-azure-identity-token}
         try {
             repository.delete(releaseName_build)
         } catch (_: Exception) {
+            // Log the exception or handle it appropriately
         }
 
     }
