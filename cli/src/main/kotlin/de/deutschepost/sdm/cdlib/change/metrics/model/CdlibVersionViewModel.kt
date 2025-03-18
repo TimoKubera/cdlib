c36274d95154b95e7452e27a827c6e7c436a2cc0
TechDebotBot <tech-debt-bot@reply.de>
2025-03-18 21:52:32 +0100

    TechDebtBot-refactor:[kotlin:S1135]Remove TODO, update 'supported' type

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/change/metrics/model/CdlibVersionViewModel.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/change/metrics/model/CdlibVersionViewModel.kt
@@ -1,10 +1,25 @@
 package de.deutschepost.sdm.cdlib.change.metrics.model
 
+import de.deutschepost.sdm.cdlib.change.metrics.model.CdlibVersionConfig
 data class CdlibVersionViewModel(
     val cdlib: String,
     val cliReleaseVersion: String,
     val cliContainerTag: String,
-    val supported: Boolean? //TODO make non nullable with CDlib 7
+    package de.deutschepost.sdm.cdlib.change.metrics.model
+    
+    data class CdlibVersionViewModel(
+        val cdlib: String,
+        val cliReleaseVersion: String,
+        val cliContainerTag: String,
+        val supported: Boolean // Changed from Boolean? to Boolean
+    ) {
+        constructor(cdlibVersionConfig: CdlibVersionConfig, supported: Boolean) : this(
+            cdlib = cdlibVersionConfig.cdlibVersion,
+            cliReleaseVersion = cdlibVersionConfig.cliReleaseVersion,
+            cliContainerTag = cdlibVersionConfig.cliContainerTag,
+            supported = supported
+        )
+    }
 ) {
     constructor(cdlibVersionConfig: CdlibVersionConfig, supported: Boolean) : this(
         cdlib = cdlibVersionConfig.cdlibVersion,
