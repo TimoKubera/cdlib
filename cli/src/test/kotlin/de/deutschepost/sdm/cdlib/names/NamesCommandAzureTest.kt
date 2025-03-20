adbcb71c0acb6925d9d44af59877bc784a33cf62
TechDebotBot <tech-debt-bot@reply.de>
2025-03-20 17:45:11 +0100

    TechDebtBot-refactor:[kotlin:S1192]Extract string literal to constant

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/NamesCommandAzureTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/NamesCommandAzureTest.kt
@@ -4,7 +4,6 @@ import de.deutschepost.sdm.cdlib.CdlibCommand
 import de.deutschepost.sdm.cdlib.names.git.GitRepository
 import de.deutschepost.sdm.cdlib.names.git.GitRevision
 import io.kotest.core.annotation.RequiresTag
-import io.kotest.core.annotation.Tags
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.extensions.system.OverrideMode
 import io.kotest.extensions.system.SystemEnvironmentTestListener
@@ -55,75 +54,83 @@ class NamesCommandAzureTest : AnnotationSpec() {
     }
 
     @Test
-    fun testNamesCreate() {
-        val (_, output) = withStandardOutput {
-            val args = "names create".toArgsArray()
-            PicocliRunner.run(CdlibCommand::class.java, *args)
+    class TestClass {
+        companion object {
+            private const val CDLIB_APP_NAME_VARIABLE = "##vso[task.setvariable variable=CDLIB_APP_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends"
         }
-
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_APP_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_EFFECTIVE_BRANCH_NAME;isOutput=true]i593_test"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ID;isOutput=true]a5c5bc3ce1907e844490697b9aa22c4196c5d781"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends_"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_REVISION;isOutput=true]a5c5bc3"
-    }
-
-    @Test
-    fun testNamesCreateOverrideOrigin() {
-        val origin = "https://git.dhl.com/Overriden/Origin.git"
-        val (_, output) = withStandardOutput {
-            val args = "names create --override-origin $origin".toArgsArray()
-            PicocliRunner.run(CdlibCommand::class.java, *args)
-        }
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ORIGIN;isOutput=true]$origin"
-    }
-
-    @Test
-    fun testNamesCreateWithValidReleaseName() {
-        val (_, output) = withStandardOutput {
-            val releaseName = "ICTO-3339_SDM-phippyandfriends_20211203.1809.18_20210908.16_a5c5bc3"
-            val args = "names create --from-release-name $releaseName".toArgsArray()
-            PicocliRunner.run(CdlibCommand::class.java, *args)
-        }
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_APP_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_APP_VERSION;isOutput=true]20211203.1809.18"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_BUILD_NUMBER;isOutput=true]20210908.16"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_EFFECTIVE_BRANCH_NAME;isOutput=true]i593_test"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ID;isOutput=true]a5c5bc3ce1907e844490697b9aa22c4196c5d781"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends_"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_VERSION;isOutput=true]20211203.1809.18_20210908.16_a5c5bc3"
-        output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_REVISION;isOutput=true]a5c5bc3"
-    }
-
-    @Test
-    fun testNamesCreateWithInvalidReleaseName() {
-        val releaseName = "ICTO-20211203.1809.18_12657_a5c5bc3"
-        val args = "--from-release-name $releaseName".toArgsArray()
-
-        val returnCode = PicocliRunner.call(NamesCommand.CreateCommand::class.java, *args)
-        returnCode shouldBeEqualComparingTo -1
-    }
-
-    @Test
-    fun testPREffectiveBranchName() {
-        withEnvironment(
-            mapOf(
-                "SYSTEM_PULLREQUEST_SOURCEBRANCH" to "refs/heads/i593_test",
-                "BUILD_SOURCEBRANCHNAME" to "merge",
-            ),
-            OverrideMode.SetOrOverride
-        ) {
+    
+        @Test
+        fun testNamesCreate() {
             val (_, output) = withStandardOutput {
                 val args = "names create".toArgsArray()
                 PicocliRunner.run(CdlibCommand::class.java, *args)
             }
-
-            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_APP_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends"
+    
+            output shouldContainIgnoringCase CDLIB_APP_NAME_VARIABLE
             output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_EFFECTIVE_BRANCH_NAME;isOutput=true]i593_test"
             output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ID;isOutput=true]a5c5bc3ce1907e844490697b9aa22c4196c5d781"
             output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends_"
             output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_REVISION;isOutput=true]a5c5bc3"
         }
+    
+        @Test
+        fun testNamesCreateOverrideOrigin() {
+            val origin = "https://git.dhl.com/Overriden/Origin.git"
+            val (_, output) = withStandardOutput {
+                val args = "names create --override-origin $origin".toArgsArray()
+                PicocliRunner.run(CdlibCommand::class.java, *args)
+            }
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ORIGIN;isOutput=true]$origin"
+        }
+    
+        @Test
+        fun testNamesCreateWithValidReleaseName() {
+            val (_, output) = withStandardOutput {
+                val releaseName = "ICTO-3339_SDM-phippyandfriends_20211203.1809.18_20210908.16_a5c5bc3"
+                val args = "names create --from-release-name $releaseName".toArgsArray()
+                PicocliRunner.run(CdlibCommand::class.java, *args)
+            }
+            output shouldContainIgnoringCase CDLIB_APP_NAME_VARIABLE
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_APP_VERSION;isOutput=true]20211203.1809.18"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_BUILD_NUMBER;isOutput=true]20210908.16"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_EFFECTIVE_BRANCH_NAME;isOutput=true]i593_test"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ID;isOutput=true]a5c5bc3ce1907e844490697b9aa22c4196c5d781"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends_"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_VERSION;isOutput=true]20211203.1809.18_20210908.16_a5c5bc3"
+            output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_REVISION;isOutput=true]a5c5bc3"
+        }
+    
+        @Test
+        fun testNamesCreateWithInvalidReleaseName() {
+            val releaseName = "ICTO-20211203.1809.18_12657_a5c5bc3"
+            val args = "--from-release-name $releaseName".toArgsArray()
+    
+            val returnCode = PicocliRunner.call(NamesCommand.CreateCommand::class.java, *args)
+            returnCode shouldBeEqualComparingTo -1
+        }
+    
+        @Test
+        fun testPREffectiveBranchName() {
+            withEnvironment(
+                mapOf(
+                    "SYSTEM_PULLREQUEST_SOURCEBRANCH" to "refs/heads/i593_test",
+                    "BUILD_SOURCEBRANCHNAME" to "merge",
+                ),
+                OverrideMode.SetOrOverride
+            ) {
+                val (_, output) = withStandardOutput {
+                    val args = "names create".toArgsArray()
+                    PicocliRunner.run(CdlibCommand::class.java, *args)
+                }
+    
+                output shouldContainIgnoringCase CDLIB_APP_NAME_VARIABLE
+                output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_EFFECTIVE_BRANCH_NAME;isOutput=true]i593_test"
+                output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_PM_GIT_ID;isOutput=true]a5c5bc3ce1907e844490697b9aa22c4196c5d781"
+                output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_RELEASE_NAME;isOutput=true]ICTO-3339_SDM-phippyandfriends_"
+                output shouldContainIgnoringCase "##vso[task.setvariable variable=CDLIB_REVISION;isOutput=true]a5c5bc3"
+            }
+        }
+    }
 
     }
 }
