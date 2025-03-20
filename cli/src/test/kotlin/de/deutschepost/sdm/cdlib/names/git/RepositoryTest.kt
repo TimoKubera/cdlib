8d8bb255ae21b5872bbadc7a3d992333452a07c1
TechDebotBot <tech-debt-bot@reply.de>
2025-03-20 17:39:53 +0100

    TechDebtBot-refactor:[kotlin:S1192] Replaced email literal with constant

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/git/RepositoryTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/git/RepositoryTest.kt
@@ -1,5 +1,6 @@
 package de.deutschepost.sdm.cdlib.names.git
 
+import io.kotest.core.annotation.BeforeAll
 import io.kotest.core.annotation.RequiresTag
 import io.kotest.core.annotation.Tags
 import io.kotest.core.spec.style.AnnotationSpec
@@ -21,23 +22,26 @@ class RepositoryTest : AnnotationSpec() {
             id = "a5c5bc3ce1907e844490697b9aa22c4196c5d781",
             longMessage = "Dummy Commit",
             shortMessage = "Dummy Commit",
-            authorName = "Firstname Lastname",
-            authorEmail = "f.l@dhl.com",
-            committerName = "Firstname Lastname",
-            committerEmail = "f.l@dhl.com"
-        )
-    }
-
-    @Test
-    fun testLastCommit() {
-        val revision = GitRepository.lastBranchCommit(currDir)
-        revision.id shouldBeEqualComparingTo "a5c5bc3ce1907e844490697b9aa22c4196c5d781"
-        revision.longMessage shouldBeEqualComparingTo "Dummy Commit"
-        revision.shortMessage shouldBeEqualComparingTo "Dummy Commit"
-        revision.authorName shouldBeEqualComparingTo "Firstname Lastname"
-        revision.authorEmail shouldBeEqualComparingTo "f.l@dhl.com"
-        revision.committerName shouldBeEqualComparingTo "Firstname Lastname"
-        revision.committerEmail shouldBeEqualComparingTo "f.l@dhl.com"
+            class RepositoryTest : AnnotationSpec() {
+                companion object {
+                    const val AUTHOR_EMAIL = "f.l@dhl.com"
+                }
+                private val currDir = System.getProperty("user.dir")
+            
+                @BeforeAll
+                fun initMocks() {
+                    mockkObject(GitRepository)
+                    every { GitRepository.getRemoteUrl(any()) } returns "https://git.dhl.com/CDLib/CDlib.git"
+                    every { GitRepository.lastBranchCommit(any()) } returns GitRevision(
+                        id = "a5c5bc3ce1907e844490697b9aa22c4196c5d781",
+                        longMessage = "Dummy Commit",
+                        shortMessage = "Dummy Commit",
+                        authorName = "Firstname Lastname",
+                        authorEmail = AUTHOR_EMAIL,
+                        committerName = "Firstname Lastname",
+                        committerEmail = AUTHOR_EMAIL
+                    )
+                }
     }
 
     @Test
