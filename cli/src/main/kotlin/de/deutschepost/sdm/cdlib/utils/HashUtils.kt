0ca0dc43d539568298d0952f10b31daaebc7a615
TechDebotBot <tech-debt-bot@reply.de>
2025-03-20 17:38:52 +0100

    TechDebtBot-refactor:[kotlin:S1192] Replace duplicated "SHA-256" with constant.

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
@@ -3,17 +3,23 @@ package de.deutschepost.sdm.cdlib.utils
 import java.io.File
 import java.security.MessageDigest
 
+class Utils {
+    companion object {
+        const val HASH_ALGORITHM = "SHA-256"
+    }
+}
+
 fun String.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(Utils.HASH_ALGORITHM)
     .digest(this.toByteArray())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun File.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(Utils.HASH_ALGORITHM)
     .digest(this.readBytes())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun ByteArray.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(Utils.HASH_ALGORITHM)
     .digest(this)
     .fold("") { str, it -> str + "%02x".format(it) }
