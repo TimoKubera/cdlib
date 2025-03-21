340a334b7b1d2f8c752ec60195b600fe2fc9fa98
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 11:51:49 +0100

    TechDebtBot-refactor:[kotlin:S1192]Centralize duplicated string into constant.

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/NameResolverAzureTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/names/NameResolverAzureTest.kt
@@ -36,31 +36,34 @@ class NameResolverAzureTest(
         SystemEnvironmentTestListener(
             mapOf(
                 "BUILD_BUILDID" to "12657",
-                "BUILD_DEFINITIONNAME" to "ICTO-3339_SDM-phippyandfriends",
+                "BUILD_DEFINITIONNAME" to Companion.TEST_DEFINITION_NAME,
                 "BUILD_SOURCEBRANCHNAME" to "i593_test",
                 "SYSTEM_COLLECTIONURI" to "https://dev.azure.com/sw-zustellung-31b3183/",
                 "SYSTEM_TEAMPROJECT" to "ICTO-3339_SDM",
                 "TF_BUILD" to "True",
                 "SYSTEM_DEFINITIONID" to "912345",
                 "SYSTEM_PULLREQUEST_SOURCEBRANCH" to "i593_test"
-            ),
-            OverrideMode.SetOrOverride
-        )
-    )
-
-    @BeforeAll
-    fun initMocks() {
-        mockkObject(GitRepository)
-        every { GitRepository.getRemoteUrl(any()) } returns "https://git.dhl.com/CDLib/CDLib.git"
-        every { GitRepository.lastBranchCommit(any()) } returns GitRevision(
-            id = "a5c5bc3ce1907e844490697b9aa22c4196c5d781",
-            longMessage = "Dummy \"Commit\"",
-            shortMessage = "Dummy \$Commit",
-            authorName = "Firstname Author",
-            authorEmail = "f.a@dhl.com",
-            committerName = "Firstname Committer",
-            committerEmail = "f.c@dhl.com"
-        )
+                        ),
+                        OverrideMode.SetOrOverride
+                    )
+                
+                    @BeforeAll
+                    fun initMocks() {
+                        mockkObject(GitRepository)
+                        every { GitRepository.getRemoteUrl(any()) } returns "https://git.dhl.com/CDLib/CDLib.git"
+                        every { GitRepository.lastBranchCommit(any()) } returns GitRevision(
+                id = "a5c5bc3ce1907e844490697b9aa22c4196c5d781",
+                longMessage = "Dummy \"Commit\"",
+                shortMessage = "Dummy \$Commit",
+                authorName = "Firstname Author",
+                authorEmail = "f.a@dhl.com",
+                committerName = "Firstname Committer",
+                committerEmail = "f.c@dhl.com"
+                        )
+                
+                    companion object {
+                        const val TEST_DEFINITION_NAME = "ICTO-3339_SDM-phippyandfriends"
+                    }
     }
 
     @Test
