95a46c93c537dc6565befc5a8d63146e79e83939
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 14:32:32 +0100

    TechDebtBot-refactor:[kotlin:S6624]Use variables for dependency versions

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -54,10 +54,10 @@ dependencies {
 
     // webapproval sharepoint
     const val httpclientVersion = "4.5.14"
+    const val jcifsVersion = "1.3.18.3"
     
-    // code range fix:
     implementation("org.apache.httpcomponents:httpclient:$httpclientVersion")
-    implementation("org.codelibs:jcifs:1.3.18.3") // Never update this, Version 2 is incompatible
+    implementation("org.codelibs:jcifs:$jcifsVersion")
     // JGit (https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit)
     implementation("org.eclipse.jgit:org.eclipse.jgit:7.1.0.202411261347-r")
     //publish metrics to azure cosmosdb
