8dd301bd5d29fda5d9bf1fb8b52b71c3e3cec9b7
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 11:49:50 +0100

    TechDebtBot-refactor:[kotlin:S1192]Replace duplicated string literals with constant

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
@@ -3,17 +3,21 @@ package de.deutschepost.sdm.cdlib.utils
 import java.io.File
 import java.security.MessageDigest
 
+object SHAUtils {
+    const val SHA_256 = "SHA-256"
+}
+
 fun String.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHAUtils.SHA_256)
     .digest(this.toByteArray())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun File.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHAUtils.SHA_256)
     .digest(this.readBytes())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun ByteArray.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHAUtils.SHA_256)
     .digest(this)
     .fold("") { str, it -> str + "%02x".format(it) }
