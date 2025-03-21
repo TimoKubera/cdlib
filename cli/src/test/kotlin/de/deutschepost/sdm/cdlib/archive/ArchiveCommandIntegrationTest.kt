67df53af59bd5ff5df99e41cc74e3802e23b9a0f
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 10:41:35 +0100

    TechDebtBot-fix:[kotlin:S108]Handle repository exceptions

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/archive/ArchiveCommandIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/archive/ArchiveCommandIntegrationTest.kt
@@ -1,5 +1,7 @@
 package de.deutschepost.sdm.cdlib.archive
 
+import org.slf4j.LoggerFactory
+import org.slf4j.Logger
 import de.deutschepost.sdm.cdlib.CdlibCommand
 import de.deutschepost.sdm.cdlib.artifactory.AZURE_ARTIFACTORY_URL
 import de.deutschepost.sdm.cdlib.artifactory.ArtifactoryClient
@@ -173,7 +175,8 @@ class ArchiveCommandIntegrationTest(@Value("\${artifactory-azure-identity-token}
         val repository = artifactory.repository(repoName)
         try {
             repository.delete(releaseName_build)
-        } catch (_: Exception) {
+        } catch (exception: Exception) {
+            println("Error deleting repository: ${exception.message}")
         }
 
     }
