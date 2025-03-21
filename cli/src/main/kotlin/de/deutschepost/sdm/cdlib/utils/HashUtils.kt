bd322a8dc6ea17ddd20248d6cd1d95bb4ad588ff
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 15:54:37 +0100

    TechDebtBot-refactor:[kotlin:S1192] Use constant for SHA-256

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
@@ -3,17 +3,19 @@ package de.deutschepost.sdm.cdlib.utils
 import java.io.File
 import java.security.MessageDigest
 
+private const val HASH_ALGORITHM = "SHA-256"
+
 fun String.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(HASH_ALGORITHM)
     .digest(this.toByteArray())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun File.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(HASH_ALGORITHM)
     .digest(this.readBytes())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun ByteArray.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(HASH_ALGORITHM)
     .digest(this)
     .fold("") { str, it -> str + "%02x".format(it) }
