7ac2ca2d981f3df7aff4da316ac286e32abb77db
TechDebotBot <tech-debt-bot@reply.de>
2025-03-21 15:26:44 +0100

    TechDebtBot-fix:[kotlin:S108]Remove empty code block.

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
@@ -149,7 +149,7 @@ fun List<SecurityTestResult>.securityTestsVerify(
             ReportType.DAST -> securityReportVerificationResult.hasDAST = true
             ReportType.SCA -> securityReportVerificationResult.hasSCA = true
             ReportType.SAST -> securityReportVerificationResult.hasSAST = true
-            ReportType.CCA -> {}
+            
             ReportType.OTHER -> {}
             ReportType.OSLC -> {}
             ReportType.OSLC_PRE -> {}
