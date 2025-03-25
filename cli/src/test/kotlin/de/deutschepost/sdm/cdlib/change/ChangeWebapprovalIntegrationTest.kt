6dfd347b519361cfb13717283eb5662f34b4dc59
TechDebotBot <tech-debt-bot@reply.de>
2025-03-25 10:52:15 +0100

    TechDebtBot-refactor:[kotlin:S117]Update parameter names to camelCase

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/ChangeWebapprovalIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/ChangeWebapprovalIntegrationTest.kt
@@ -40,7 +40,7 @@ import java.util.*
 @MicronautTest
 class ChangeWebapprovalIntegrationTest(
     @Value("\${change-management-token}") val token: String,
-    @Value("\${sharepoint.username}") val sp_username: String,
+    @Value("\${sharepoint.username}") val spUsername: String,
     @Value("\${sharepoint.password}") val sp_password: String,
     @Value("\${artifactory-its-identity-token}") val artifactoryIdentityToken: String,
     private val cosmosDashboardRepository: CosmosDashboardRepository
