0a6e385c4e7058f671e884765e0bdf6411ebe200
TechDebotBot <tech-debt-bot@reply.de>
2025-03-25 14:02:29 +0100

    TechDebtBot-refactor:[kotlin:S117] Updated parameter names to camelCase

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/ChangeFullIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/ChangeFullIntegrationTest.kt
@@ -41,12 +41,13 @@ import java.time.temporal.ChronoUnit
 @MicronautTest
 class ChangeFullIntegrationTest(
     @Value("\${change-management-token}") val token: String,
-    @Value("\${sharepoint.username}") val sp_username: String,
-    @Value("\${sharepoint.password}") val sp_password: String,
+    @Value("\${sharepoint.username}") val spUsername: String,
+    @Value("\${sharepoint.password}") val spPassword: String,
     @Value("\${artifactory-its-identity-token}") val artifactoryIdentityToken: String,
     private val changeTestHelper: ChangeTestHelper,
     private val changeHandler: ChangeHandler,
     private val cosmosDashboardRepository: CosmosDashboardRepository
+
 ) : FunSpec() {
     private val timestamp =
         DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(ZonedDateTime.now().truncatedTo(ChronoUnit.SECONDS))
