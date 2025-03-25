5c7c54f065d3fb2333f88fd2b9e7f59751314619
TechDebotBot <tech-debt-bot@reply.de>
2025-03-25 10:59:29 +0100

    TechDebtBot-refactor:[kotlin:S1192]Use constant instead of duplicating "Policy Profile: Distribution"

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/release/ReportOslcGradlePluginIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/release/ReportOslcGradlePluginIntegrationTest.kt
@@ -42,6 +42,8 @@ class ReportOslcGradlePluginIntegrationTest(
     )
 
     init {
+        private val POLICY_PROFILE_DISTRIBUTION = "Policy Profile: Distribution"
+    
         context("Check OSLC-Plugin report and upload") {
             test("Check OSLC-Plugin report locally") {
                 val args = "--debug --files $jsonFile --no-distribution".toArgsArray()
@@ -51,7 +53,7 @@ class ReportOslcGradlePluginIntegrationTest(
                 ret shouldBeExactly 0
                 output shouldContain "Policy Profile: Non-Distribution"
             }
-
+    
             test("Upload OSLC-Plugin report to artifactory") {
                 val args =
                     "--debug --files $jsonFile --no-distribution --artifactory-its-instance --artifactory-identity-token $artifactoryIdentityToken --repo-name $repoName --type build".toArgsArray()
@@ -61,17 +63,17 @@ class ReportOslcGradlePluginIntegrationTest(
                 ret shouldBeExactly 0
                 output shouldContain "Uploaded Artifact:"
             }
-
+    
             test("Check OSLC-Plugin report locally should fail due to distribution flag") {
                 val args = "--debug --files $jsonFile --distribution".toArgsArray()
                 val (ret, output) = withStandardOutput {
                     PicocliRunner.call(ReportCommand.CheckCommand::class.java, *args)
                 }
                 ret shouldBeExactly -1
-                output shouldContain "Policy Profile: Distribution"
+                output shouldContain POLICY_PROFILE_DISTRIBUTION
                 output shouldNotContain "Unapproved Licenses Count: 0"
             }
-
+    
             test("Check OSLC-Plugin report locally should succeed with accepted list") {
                 val args =
                     "--debug --files $jsonFile --distribution --oslc-accepted-list $acceptedListFile".toArgsArray()
@@ -79,10 +81,10 @@ class ReportOslcGradlePluginIntegrationTest(
                     PicocliRunner.call(ReportCommand.CheckCommand::class.java, *args)
                 }
                 ret shouldBeExactly 0
-                output shouldContain "Policy Profile: Distribution"
+                output shouldContain POLICY_PROFILE_DISTRIBUTION
                 output shouldContain "Unapproved Licenses Count: 0"
             }
-
+    
             test("Check OSLC-Plugin report locally should fail with accepted list because of wrong license") {
                 val args =
                     "--debug --files $jsonFile --distribution --oslc-accepted-list $acceptedListFileWrongLicense".toArgsArray()
@@ -90,11 +92,11 @@ class ReportOslcGradlePluginIntegrationTest(
                     PicocliRunner.call(ReportCommand.CheckCommand::class.java, *args)
                 }
                 ret shouldBeExactly -1
-                output shouldContain "Policy Profile: Distribution"
+                output shouldContain POLICY_PROFILE_DISTRIBUTION
                 output shouldContain "Unapproved Licenses Count: 1"
                 output shouldContain "Alladin Free Public License 9: [com.rabbitmq:amqp-client]"
             }
-
+    
             test("Check OSLC-Plugin report locally should fail with accepted list because of wrong version number") {
                 val args =
                     "--debug --files $jsonFile --distribution --oslc-accepted-list $acceptedListFileWrongVersion".toArgsArray()
@@ -102,9 +104,12 @@ class ReportOslcGradlePluginIntegrationTest(
                     PicocliRunner.call(ReportCommand.CheckCommand::class.java, *args)
                 }
                 ret shouldBeExactly -1
-                output shouldContain "Policy Profile: Distribution"
+                output shouldContain POLICY_PROFILE_DISTRIBUTION
                 output shouldContain "Unapproved Licenses Count: 1"
                 output shouldContain "Common Development and Distribution License 1.0: [org.apache.tomcat.embed:tomcat-embed-core]"
+            }
+        }
+    }
             }
 
             test("Trying to upload failing OSLC-Plugin report to artifactory should missing") {
