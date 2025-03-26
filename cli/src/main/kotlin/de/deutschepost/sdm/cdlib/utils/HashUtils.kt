d1f883f542bf8a4b9ea3c1790c8263e8d34ed794
TechDebotBot <tech-debt-bot@reply.de>
2025-03-26 17:12:16 +0100

    TechDebtBot-refactor:[kotlin:S1192]Define SHA_256

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/utils/HashUtils.kt
@@ -2,18 +2,19 @@ package de.deutschepost.sdm.cdlib.utils
 
 import java.io.File
 import java.security.MessageDigest
+private const val SHA_256 = "SHA-256"
 
 fun String.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHA_256)
     .digest(this.toByteArray())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun File.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHA_256)
     .digest(this.readBytes())
     .fold("") { str, it -> str + "%02x".format(it) }
 
 fun ByteArray.sha256sum(): String = MessageDigest
-    .getInstance("SHA-256")
+    .getInstance(SHA_256)
     .digest(this)
     .fold("") { str, it -> str + "%02x".format(it) }
