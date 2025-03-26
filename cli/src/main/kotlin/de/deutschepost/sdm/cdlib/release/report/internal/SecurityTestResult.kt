aa23b119036641fd93eec9a135587dd721c4f693
TechDebotBot <tech-debt-bot@reply.de>
2025-03-26 16:43:47 +0100

    TechDebtBot-refactor:[kotlin:S108]Clarify block

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
@@ -149,7 +149,7 @@ fun List<SecurityTestResult>.securityTestsVerify(
             ReportType.DAST -> securityReportVerificationResult.hasDAST = true
             ReportType.SCA -> securityReportVerificationResult.hasSCA = true
             ReportType.SAST -> securityReportVerificationResult.hasSAST = true
-            ReportType.CCA -> {}
+            ReportType.CCA -> { // Intentionally left empty: no action required for CCA report type }
             ReportType.OTHER -> {}
             ReportType.OSLC -> {}
             ReportType.OSLC_PRE -> {}
