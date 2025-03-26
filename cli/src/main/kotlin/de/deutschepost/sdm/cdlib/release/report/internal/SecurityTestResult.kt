c100d6cc4a1e10ea997a702bec7a4dc0c17d6882
TechDebotBot <tech-debt-bot@reply.de>
2025-03-26 15:25:04 +0100

    TechDebtBot-chore:[kotlin:S108]Clarify intentional empty block

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
@@ -149,7 +149,7 @@ fun List<SecurityTestResult>.securityTestsVerify(
             ReportType.DAST -> securityReportVerificationResult.hasDAST = true
             ReportType.SCA -> securityReportVerificationResult.hasSCA = true
             ReportType.SAST -> securityReportVerificationResult.hasSAST = true
-            ReportType.CCA -> {}
+            ReportType.CCA -> { /* This block is intentionally left empty as there is currently no implementation needed for CCA report type. */ }
             ReportType.OTHER -> {}
             ReportType.OSLC -> {}
             ReportType.OSLC_PRE -> {}
