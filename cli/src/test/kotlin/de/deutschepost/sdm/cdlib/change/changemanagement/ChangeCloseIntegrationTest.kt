2c3a4bd60f3a2034dc27f483097f701a29d4cc50
TechDebotBot <tech-debt-bot@reply.de>
2025-04-10 12:08:52 +0200

    TechDebtBot-refactor:[kotlin:S1192]Extract const

--- cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/changemanagement/ChangeCloseIntegrationTest.kt
+++ cli/src/test/kotlin/de/deutschepost/sdm/cdlib/change/changemanagement/ChangeCloseIntegrationTest.kt
@@ -216,6 +216,8 @@ class ChangeCloseIntegrationTest(
         }
     }
 
+        companion object { const val DASHBOARD_STATUS_CODE_MESSAGE = "Dashboard status code: 201" }
+        
         "Closing change successfully publishes metrics via Jenkins" {
             withEnvironment(
                 "CDLIB_JOB_URL" to "http://integration-test-url.jenkuns.example.com/foo/bar/job/1337",
@@ -225,20 +227,20 @@ class ChangeCloseIntegrationTest(
                     .post(changeDetails)
                     .preauthorize()
                     .transition(OPEN_TO_IMPLEMENTATION)
-
+        
                 val (exitCode, output) = withStandardOutput {
                     PicocliRunner.call(
                         ChangeCommand.CloseCommand::class.java,
                         *"--test --jira-token $token --commercial-reference $commercialReference --status $status".toArgsArray()
                     )
                 }
-
+        
                 output shouldContain "http://integration-test-url.jenkuns.example.com"
-                output shouldContain "Dashboard status code: 201"
+                output shouldContain DASHBOARD_STATUS_CODE_MESSAGE
                 exitCode shouldBeExactly 0
             }
         }
-
+        
         "Closing change successfully publishes metrics via Jenkins as infrastructure deployment" {
             withEnvironment(
                 "CDLIB_JOB_URL" to "http://integration-test-url.jenkuns.example.com/foo/bar/job/1337",
@@ -248,28 +250,28 @@ class ChangeCloseIntegrationTest(
                     .post(changeDetails)
                     .preauthorize()
                     .transition(OPEN_TO_IMPLEMENTATION)
-
+        
                 val (exitCode, output) = withStandardOutput {
                     PicocliRunner.call(
                         ChangeCommand.CloseCommand::class.java,
                         *"--test --token $token --commercial-reference $commercialReference --status $status --deployment-type INFRA".toArgsArray()
                     )
                 }
-
+        
                 output shouldContain "http://integration-test-url.jenkuns.example.com"
-                output shouldContain "Dashboard status code: 201"
+                output shouldContain DASHBOARD_STATUS_CODE_MESSAGE
                 output shouldContain "INFRA"
                 exitCode shouldBeExactly 0
             }
         }
-
+        
         "Closing change successfully publishes metrics via AzureDevOps" {
             val rnd = UUID.randomUUID().toString().substringBefore("-")
-
+        
             withEnvironment(
                 mapOf(
                     "CDLIB_JOB_URL" to "https://dev.azure.com/sw-zustellung-$rnd/ICTO-3339_SDM-phippyandfriends",
-                    "CDLIB_PIPELINE_URL" to "https://dev.azure.com/sw-zustellung-$rnd/ICTO-3339_SDM-phippyandfriends/_build?definitionId=1337&branchFilter=superFeature",
+                    "CDLIB_PIPELINE_URL" to "https://dev.azure.com/sw-zustellung-$rnd/ICTO-3339_SDM-phippyandfriends/_build?definitionId=1337&branchFilter=superFeature"
                 ),
                 OverrideMode.SetOrOverride
             ) {
@@ -277,18 +279,19 @@ class ChangeCloseIntegrationTest(
                     .post(changeDetails)
                     .preauthorize()
                     .transition(OPEN_TO_IMPLEMENTATION)
-
+        
                 val (exitCode, output) = withStandardOutput {
                     PicocliRunner.call(
                         ChangeCommand.CloseCommand::class.java,
                         *"--test --jira-token $token --commercial-reference $commercialReference --status $status".toArgsArray()
                     )
                 }
-
+        
                 output shouldContain "https://dev.azure.com/sw-zustellung-$rnd"
-                output shouldContain "Dashboard status code: 201"
+                output shouldContain DASHBOARD_STATUS_CODE_MESSAGE
                 exitCode shouldBeExactly 0
             }
+        }
         }
 
         "Closing change successfully when having a fuzzy matching job url" {
