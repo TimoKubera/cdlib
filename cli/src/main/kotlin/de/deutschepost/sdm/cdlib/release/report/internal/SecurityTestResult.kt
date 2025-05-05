be96749c0eb1a4a71a5b75263a076ed8347f67fe
TechDebotBot <tech-debt-bot@reply.de>
2025-05-05 09:27:23 +0200

    TechDebtBot-refactor:[kotlin:S108]No-op comment

--- cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
+++ cli/src/main/kotlin/de/deutschepost/sdm/cdlib/release/report/internal/SecurityTestResult.kt
@@ -149,7 +149,7 @@ fun List<SecurityTestResult>.securityTestsVerify(
             ReportType.DAST -> securityReportVerificationResult.hasDAST = true
             ReportType.SCA -> securityReportVerificationResult.hasSCA = true
             ReportType.SAST -> securityReportVerificationResult.hasSAST = true
-            ReportType.CCA -> {}
+            ReportType.CCA -> { // No operation needed for CCA }
             ReportType.OTHER -> {}
             ReportType.OSLC -> {}
             ReportType.OSLC_PRE -> {}
